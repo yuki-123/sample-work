abd1493444990b5a779985a8b265787afb158108
yuki-123 <30871498+yuki-123@users.noreply.github.com>
2017-10-16 03:33:34 +1100

    Add files via upload

---
+++ params.py
@@ -0,0 +1,4 @@
+test1_param = [["University of Technology Sydney","15 Broadway, Ultimo NSW 2007, Australia"],  ["University of Technology Sydney","14 Broadway, Ultimo NSW 2007, Australia"]  ]
+test2_param = ["JSON","test3","Python"]
+
+
